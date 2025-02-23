dc47118e961198fd82ec32997d308e5351e6e0fe
LABCAT <shane.watters.nz@gmail.com>
2025-02-23 18:20:38 +1300

    Adding controls to web component story.

--- apps/storybook/stories/WebComponent.stories.ts
+++ apps/storybook/stories/WebComponent.stories.ts
@@ -2,17 +2,23 @@ import { Meta, Story } from '@storybook/vue3'
 
 export default {
   title: 'Web Component',
-  parameters: {
-    controls: { disabled: true },
+  argTypes: {
+    message: { control: 'text' },
   },
 } as Meta
 
-const Template: Story = () => ({
+const Template: Story = (args) => ({
+  setup() {
+    return { args }
+  },
   template: `
     <div>
-      <my-component></my-component>
+      <my-component v-bind="args"></my-component>
     </div>
   `,
 })
 
 export const Default = Template.bind({})
+Default.args = {
+  message: 'Hello, Storybook!',
+}
